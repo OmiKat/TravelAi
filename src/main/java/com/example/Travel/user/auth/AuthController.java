dd16954ecbeb5572c5ee144e9bab0b0fe2df0522
omi <ompraveer@gmail.com>
2026-06-08 20:55:15 +0530

    auth complete

--- src/main/java/com/example/Travel/user/auth/AuthController.java
+++ src/main/java/com/example/Travel/user/auth/AuthController.java
@@ -1,4 +1,36 @@
 package com.example.Travel.user.auth;
 
+import com.example.Travel.user.auth.dto.AuthResponse;
+import com.example.Travel.user.auth.dto.LoginRequest;
+import com.example.Travel.user.auth.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest registerRequest){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(authService.register(registerRequest));
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest loginRequest){
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+
 }
--- src/main/java/com/example/Travel/user/auth/AuthService.java
+++ src/main/java/com/example/Travel/user/auth/AuthService.java
@@ -1,4 +1,68 @@
 package com.example.Travel.user.auth;
 
+import com.example.Travel.user.UserEntity;
+import com.example.Travel.user.UserRepository;
+import com.example.Travel.user.auth.dto.AuthResponse;
+import com.example.Travel.user.auth.dto.LoginRequest;
+import com.example.Travel.user.auth.dto.RegisterRequest;
+import com.example.Travel.user.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request){
+
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("User is already Register");
+        }
+
+        UserEntity user = UserEntity.builder()
+                .userName(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .email(user.getEmail())
+                .name(user.getUsername())
+                .build();
+
+    }
+
+    public AuthResponse login(LoginRequest request){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+        ));
+
+        UserEntity user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        String token = jwtService.generateToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .email(user.getEmail())
+                .name(user.getUsername())
+                .build();
+        
+    }
+
+
+
 }
